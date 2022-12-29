c588fd2455be80b197816d823b4a772a32cf01cc
Theia Vogel <theia@vgel.me>
2022-12-28 19:54:08 -0800

    assignment expressions w/ place vs value distinction

--- compiler.py
+++ compiler.py
@@ -212,49 +212,62 @@ def emit_return(frame: StackFrame) -> None:
     print(f"    return")
 
 
-def emit_load(frame: StackFrame, name: Token) -> None:
-    print(f"    ;; load {name.content}")
-    print(f"    global.get $__stack_pointer")
-    print(f"    i32.const {frame.get_offset(name)}")
-    print(f"    i32.add")
-    print(f"    i32.load")
+class ExprResultKind(enum.Enum):
+    Value = "Value"
+    # a place (corresponding to an address) that can be loaded from / stored to
+    # not all addresses are places, e.g. &x is a value (&x = 1 is meaningless)
+    Place = "Place"
 
 
-def expression(lexer: Lexer, frame: StackFrame) -> None:
-    def value() -> None:
+def load_result(erk: ExprResultKind) -> None:
+    if erk == ExprResultKind.Place:
+        print("    i32.load")
+
+
+def expression(lexer: Lexer, frame: StackFrame) -> ExprResultKind:
+    def value() -> ExprResultKind:
         if const := lexer.try_next(TokenKind.IntConst):
             print(f"    i32.const {const.content}")
+            return ExprResultKind.Value
         elif varname := lexer.try_next(TokenKind.Name):
-            emit_load(frame, varname)
+            print(f"    ;; load {varname.content}")
+            print(f"    global.get $__stack_pointer")
+            print(f"    i32.const {frame.get_offset(varname)}")
+            print(f"    i32.add")
+            return ExprResultKind.Place
         elif lexer.try_next(TokenKind.Minus):
             if lexer.peek().kind == TokenKind.Minus:
                 die("predecrement not supported", lexer.line)
-            value()
+            load_result(value())
             print(f"    i32.neg")
+            return ExprResultKind.Value
         elif lexer.try_next(TokenKind.Star):
-            expression(lexer, frame)
-            print(f"    i32.load")
+            load_result(value())
+            return ExprResultKind.Place
         elif lexer.try_next(TokenKind.Ampersand):
-            name = lexer.next(TokenKind.Name)
-            print(f"    ;; &{name.content}")
-            print(f"    global.get $__stack_pointer")
-            print(f"    i32.const {frame.get_offset(name)}")
-            print(f"    i32.add")
+            expr_kind = value()
+            if expr_kind != ExprResultKind.Place:
+                die("cannot take reference to value", lexer.line)
+            return ExprResultKind.Value
         elif lexer.try_next(TokenKind.OpenParen):
-            expression(lexer, frame)
+            expr_kind = expression(lexer, frame)
             lexer.next(TokenKind.CloseParen)
+            return expr_kind
         else:
             die("expected value", lexer.line)
 
     def makeop(
-        higher: Callable[[], None], ops: dict[TokenKind, str]
-    ) -> Callable[[], None]:
-        def op() -> None:
-            higher()
+        higher: Callable[[], ExprResultKind], ops: dict[TokenKind, str]
+    ) -> Callable[[], ExprResultKind]:
+        def op() -> ExprResultKind:
+            higher_kind = higher()
             if lexer.peek().kind in ops.keys():
+                load_result(higher_kind)
                 op = lexer.next()
-                higher()
+                load_result(higher())
                 print(f"    {ops[op.kind]}")
+                return ExprResultKind.Value
+            return higher_kind
 
         return op
 
@@ -272,7 +285,19 @@ def expression(lexer: Lexer, frame: StackFrame) -> None:
     bitor = makeop(bitand, {TokenKind.Pipe: "i32.or"})
     xor = makeop(bitor, {TokenKind.Caret: "i32.xor"})
 
-    xor()
+    def assign() -> ExprResultKind:
+        lhs_kind = xor()
+        if lexer.try_next(TokenKind.Equals):
+            if lhs_kind != ExprResultKind.Place:
+                die("lhs of assignment cannot be value", lexer.line)
+            print(f"    call $__dup_i32")  # save addr
+            load_result(assign())
+            print(f"    i32.store")
+            print(f"    i32.load")  # use dup'd addr
+            return ExprResultKind.Value
+        return lhs_kind
+
+    return assign()
 
 
 def bracketed_block_or_single_statement(lexer: Lexer, frame: StackFrame) -> None:
@@ -286,36 +311,15 @@ def bracketed_block_or_single_statement(lexer: Lexer, frame: StackFrame) -> None
 def statement(lexer: Lexer, frame: StackFrame) -> None:
     if lexer.try_next(TokenKind.Return):
         if lexer.peek().kind != TokenKind.Semicolon:
-            expression(lexer, frame)
+            load_result(expression(lexer, frame))
         lexer.next(TokenKind.Semicolon)
         emit_return(frame)
-    elif lexer.peek().kind in (TokenKind.Name, TokenKind.Star):
-        # parse lhs
-        pointer_level = 0
-        while lexer.try_next(TokenKind.Star):
-            pointer_level += 1
-        name = lexer.next(TokenKind.Name)
-        _ = frame.get_offset(name)  # assert var exists in case of `x;` with no store
-        print(f"    ;; lhs {'*' * pointer_level}{name.content}")
-        print(f"    global.get $__stack_pointer")
-        print(f"    i32.const {frame.get_offset(name)}")
-        print(f"    i32.add")
-        for _ in range(pointer_level):
-            print(f"    i32.load")
-        if lexer.try_next(TokenKind.Equals):
-            print(f"    ;; rhs {'*' * pointer_level}{name.content}")
-            expression(lexer, frame)
-            print(f"    ;; store {'*' * pointer_level}{name.content}")
-            print(f"    i32.store")
-        else:
-            print(f"    drop")  # dead load ¯\_(ツ)_/¯
-        lexer.next(TokenKind.Semicolon)
     elif lexer.try_next(TokenKind.If):
         print("    ;; if")
         print("    block")  # for else
         print("    block")
         lexer.next(TokenKind.OpenParen)
-        expression(lexer, frame)
+        load_result(expression(lexer, frame))
         lexer.next(TokenKind.CloseParen)
         print("    i32.eqz")
         print("    br_if 0")  # exit into else block
@@ -333,17 +337,19 @@ def statement(lexer: Lexer, frame: StackFrame) -> None:
         print("block")
         print("loop")
         lexer.next(TokenKind.OpenParen)
-        expression(lexer, frame)
+        load_result(expression(lexer, frame))
         lexer.next(TokenKind.CloseParen)
         print("    i32.eqz")
-        print("    br_if 1") # exit loop by jumping forward to end of enclosing block
+        print("    br_if 1")  # exit loop by jumping forward to end of enclosing block
         print("    ;; while body")
         bracketed_block_or_single_statement(lexer, frame)
-        print("    br 0") # jump to beginning of loop
+        print("    br 0")  # jump to beginning of loop
         print("    end")
         print("    end")
     else:
-        die("expected statement", lexer.line)
+        expression(lexer, frame)
+        lexer.next(TokenKind.Semicolon)
+        print("    drop")
 
 
 def variable_declaration(lexer: Lexer, frame: StackFrame) -> None:
@@ -399,6 +405,9 @@ def compile(src: str) -> None:
     print("(module")
     print("  (memory 2)")
     print("  (global $__stack_pointer (mut i32) (i32.const 66560))")
+    print("  (func $__dup_i32 (param i32) (result i32 i32)")
+    print("    (local.get 0)")
+    print("    (local.get 0))")
 
     lexer = Lexer(src)
 
